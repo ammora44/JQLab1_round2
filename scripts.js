d0110bd84dd8889224b0cef5371ec92ef8be1a3b
amanda <am.morang@gmail.com>
2018-05-09 08:45:44 -0400

    First commit of JQLab2, almost done

--- scripts.js
+++ scripts.js
@@ -2,21 +2,23 @@
   
   $(document).ready(() => {
 
+let selectedTable;
+
 // GENERAL FORM INFO
 
   $(".table").click(() => {              // FORM POPS UP WHEN
-    $(".popup").show();                   // ANY BUTTON/TABLE IS CLICKED 
+    $(".popup").fadeIn(750);                  // ANY BUTTON/TABLE IS CLICKED 
   });
+
   $(".table").hover(function(event) {
     $(event.target).addClass("table hover")
- })
+  })
 
 // CLICKING OUT OF FORM
 
   $("#trash").click(() => {               // WHEN YOU CLICK ON X
-    $(".popup").hide();                   // FORM DISAPEARS
+    $(".popup").fadeOut(750);                   // FORM DISAPEARS
   });  
-
 
  // HIDING THE TABLE
 
@@ -27,21 +29,31 @@
 // HITTING THE SAVE BUTTON
 
 $("#save").click(() => {               // SAVE BUTTON THAT IS CLICKED 
-  $(".popup").hide(); // HIDES FORM
-  let tables = $(".table");
-  let formNumberText = $("#table-num").text();
-  let tableText = $(".table").val();
-
+  $(".popup").fadeOut(750);                // HIDES FORM
+  
+  console.log(selectedTable);
+  selectedTable.addClass("reserved");
+  let partyName = $("#name").val();        // value of name input
+  let partySize = $("#size").val();       // value of size input
+  console.log(partyName, partySize);
+  selectedTable.parent().append("<p class='hidden'>'${partyName} ${partySize}</p>");
 });
 
 // RESERVED TABLE ACTIONS
 
  $(".table").click((event) => {                      // TABLES ARE ACCESSED & COLORED 
-    $(event.target).removeClass(".table");      // INDIVIDUALLY W/A CLICK
+  
+    selectedTable = $(event.target);
     $("#table-num").text($(event.target).val())
-    $(event.target).removeClass("table hover");
-    
+   
  })
+
+
+
+
+
+
+
 
 });
 
